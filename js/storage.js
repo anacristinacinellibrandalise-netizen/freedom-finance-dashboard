ccd78478413656f9d44c61944d07c1601812cebd
anacristinacinellibrandalise-netizen <anacristinacinellibrandalise@gmail.com>
2026-03-04 12:39:42 -0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,17 @@
+const Storage = {
 
+  getData() {
+    const data = localStorage.getItem("financeData");
+    return data ? JSON.parse(data) : {
+      fluxo: [],
+      rendaFixa: [],
+      rendaVariavel: [],
+      cripto: []
+    };
+  },
+
+  saveData(data) {
+    localStorage.setItem("financeData", JSON.stringify(data));
+  }
+
+};
